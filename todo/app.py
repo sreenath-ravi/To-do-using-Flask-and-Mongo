762326ab2a02a4842eaa8eef0dd9781e91119466
Sreenath R <72565010+sreenath-ravi@users.noreply.github.com>
2022-07-11 20:04:07 +0530

    Add files via upload

---
+++ todo/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, url_for, redirect
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config['MONGO_URI'] = 'mongodb+srv://developerninja:123@cluster0.nolc9.mongodb.net/mydata?retryWrites=true&w=majority'
+mongo = PyMongo(app)
+
+todos = mongo.db.todos
+
+@app.route('/')
+def index():
+    saved_todos = todos.find()
+    return render_template('index.html', todos=saved_todos)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    new_todo = request.form.get('new-todo')
+    todos.insert_one({'text' : new_todo, 'complete' : False})
+    return redirect(url_for('index'))
+
+@app.route('/complete/<oid>')
+def complete(oid):
+    todo_item = todos.find_one({'_id': ObjectId(oid)})
+    todo_item['complete'] = True
+    todos.save(todo_item)
+    return redirect(url_for('index'))
+
+@app.route('/delete_completed')
+def delete_completed():
+    todos.delete_many({'complete' : True})
+    return redirect(url_for('index'))
+
+@app.route('/delete_all')
+def delete_all():
+    todos.delete_many({})
+    return redirect(url_for('index'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
